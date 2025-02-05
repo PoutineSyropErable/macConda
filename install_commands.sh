76743c8436d16c4aef2fee0a880541bdcea2c657
François Pagé <francois.page@mail.mcgill.ca>
2025-02-05 13:51:46 -0500

    ok, we have a mac conda installation working

--- install_commands.sh
+++ install_commands.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 
 curl -o Miniconda3-MacOSX-arm64.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
-bash Miniconda3-MacOSX-arm64.sh # press no when it ask to add it to path and activation
+bash Miniconda3-MacOSX-arm64.sh # press no when it ask to add it to path and activation. It can cause problem and i gave you two
+# alias to redo it where you shouldn't have any PATH problem
+
+zsh
+conda_activate
+conda create --name master_venv
+conda activate master_venv
+conda install -y requests websocket-client pillow tk
---
+++ readme.md
@@ -0,0 +1,13 @@
+How to install:
+
+```
+mkdir -p ~/Downloads
+[ -d "$HOME/Downloads/macConda/.git" ] || git clone --depth=1 https://github.com/PoutineSyropErable/macConda ~/Downloads/macConda
+cd ~/Downloads/macConda
+git pull origin master
+zsh
+
+
+./install_commands.sh # you might need to execute it yourself, or copy paste its commands and type them not from a script.
+
+```
