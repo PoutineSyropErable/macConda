9eb6ed355d39305c6b15891f9908710496b3734c
François Pagé <francois.page@mail.mcgill.ca>
2025-02-05 13:42:47 -0500

    conda_activate

---
+++ install_commands.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+curl -o Miniconda3-MacOSX-arm64.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+bash Miniconda3-MacOSX-arm64.sh # press no when it ask to add it to path and activation
